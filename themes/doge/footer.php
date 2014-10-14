8c6a30eb5143295572f6388fbba9f5cde2145335
Guillaume Trinh <gutrinh@gmail.com>
2014-10-14 12:54:17 +0200

    init calendar

--- themes/doge/footer.php
+++ themes/doge/footer.php
@@ -13,5 +13,28 @@
             bloc footer
         </footer>
     </div>
+
+<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/underscore-min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/calendar.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/language/fr-FR.js"></script>
+<script type="text/javascript">
+    var calendar = $("#calendar").calendar(
+        {
+            tmpl_path: "<?php echo get_template_directory_uri(); ?>/tmpls/",
+            events_source: function () { return []; },
+            language: 'fr-FR'
+        });
+
+    $('button[data-calendar-nav]').each(function() {
+        var $this = $(this);
+        $this.click(function() {
+            calendar.navigate($this.data('calendar-nav'));
+        });
+    });
+
+</script>
+
 </body>
 </html>
