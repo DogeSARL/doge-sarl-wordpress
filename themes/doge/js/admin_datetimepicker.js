f26ab8f12689efd860e3fd2d8d199a48f3a2c432
Guillaume Trinh <gutrinh@gmail.com>
2014-10-17 12:20:46 +0200

    finalisation calendar

--- themes/doge/js/admin_datetimepicker.js
+++ themes/doge/js/admin_datetimepicker.js
@@ -1,5 +1,13 @@
 jQuery(document).ready(function(){
-    jQuery("#evenement_date").datetimepicker({
+    jQuery("#evenement_date_debut").datetimepicker({
+        language: 'fr',
+        pickTime: false,
+        defaultDate: new Date()
+    });
+});
+
+jQuery(document).ready(function(){
+    jQuery("#evenement_date_fin").datetimepicker({
         language: 'fr',
         pickTime: false,
         defaultDate: new Date()
