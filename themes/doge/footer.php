72b1ac24a40aa1f1bd91e41b680cc937c7a606e9
Djex <djex@mail.com>
2014-11-05 10:30:09 +0100

    Front

--- themes/doge/footer.php
+++ themes/doge/footer.php
@@ -2,11 +2,12 @@
     <footer>
           <?php bloginfo('name'); ?> est crée par <a href="localhost">Doge</a>.
     </footer>
-</body>
+
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
 <script src="<?php bloginfo('template_url'); ?>/js/plugins.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/jquery.bxslider/jquery.bxslider.min.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
 <?php wp_footer(); ?>
+</body>
 </html>
--- themes/doge/index.php
+++ themes/doge/index.php
@@ -8,20 +8,77 @@
 		</div>
 	    <div class="content_wrapper clearfix">
 	    	<div class="col_50 wrapper_50_home">
-	    		<h2>Tendances swag du moment</h2>
-	    		<p class="subtitle">Nice</p>
+	    			<h2>Tendances swag du moment</h2>
+	    			<p class="subtitle">Nice</p>
+	    		<hr>
 	    		<div class="wrapper_blocks">
-	    			<div class="post_block">
-	    				<div class="post_image_preview"><img alt="" src="<?php bloginfo('template_url'); ?>/img/postpreview.jpg"></div>
-	    				<h3>Lorem ipsum dominum</h3>
-	    				<p>Lorem ipsum dominum blablablabla blabla blabla</p>
-	    			</div>
+					<a class="border_animate" title="" href="">
+						<div class="post_block">
+							<div class="post_image_preview border_container">
+								<div class="border_left"></div>
+								<div class="border_top"></div>
+								<div class="border_right"></div>
+								<div class="border_bottom"></div>
+								<img alt="" src="<?php bloginfo('template_url'); ?>/img/postpreview.jpg">
+							</div>
+							<h3>Lorem ipsum dominum</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							<div class="knowmore">Tu peux cliquer, so amazing -></div>
+						</div>
+					</a>
+	    			<hr class="betweenposts">
+	    			<a class="border_animate" title="" href="">
+						<div class="post_block">
+							<div class="post_image_preview border_container">
+								<div class="border_left"></div>
+								<div class="border_top"></div>
+								<div class="border_right"></div>
+								<div class="border_bottom"></div>
+								<img alt="" src="<?php bloginfo('template_url'); ?>/img/postpreview.jpg">
+							</div>
+							<h3>Lorem ipsum dominum</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							<div class="knowmore">Tu peux cliquer, so amazing -></div>
+						</div>
+					</a>
 	    		</div>
-
 	    	</div>
 	    	<div class="col_50 wrapper_50_home">
-	    		<h2>Le charisme canin</h2>
-	    		<p class="subtitle">So amazing</p>
+	    			<h2>événements canins / rencontrer une star</h2>
+	    			<p class="subtitle">Nice</p>
+	    		<hr>
+	    		<div class="wrapper_blocks">
+					<a class="border_animate" title="" href="">
+						<div class="post_block">
+							<div class="post_image_preview border_container">
+								<div class="border_left"></div>
+								<div class="border_top"></div>
+								<div class="border_right"></div>
+								<div class="border_bottom"></div>
+								<img alt="" src="<?php bloginfo('template_url'); ?>/img/postpreview.jpg">
+							</div>
+							<h3>Lorem ipsum dominum</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							<div class="knowmore">Tu peux cliquer, so amazing -></div>
+						</div>
+					</a>
+	    			<hr class="betweenposts">
+	    			<a class="border_animate" title="" href="">
+						<div class="post_block">
+							<div class="post_image_preview border_container">
+								<div class="border_left"></div>
+								<div class="border_top"></div>
+								<div class="border_right"></div>
+								<div class="border_bottom"></div>
+								<img alt="" src="<?php bloginfo('template_url'); ?>/img/postpreview.jpg">
+							</div>
+							<h3>Lorem ipsum dominum</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							<div class="knowmore">Tu peux cliquer, so amazing -></div>
+						</div>
+					</a>
+	    		</div>
 	    	</div>
 	    </div>
 	</div>
+<?php get_footer(); ?>
