5b3aba5195390b7530b0b692b8fdf3ba41dac8d7
Djex <djex@mail.com>
2014-11-16 21:26:46 +0100

    correct sur header

--- themes/doge/header.php
+++ themes/doge/header.php
@@ -50,7 +50,7 @@
                 <!-- <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" /> -->
 
                 <ul class="menu clearfix">
-                    <li><a href="">Accueil</a></li>
+                    <li><a href="<?php bloginfo('url'); ?>">Accueil</a></li>
                     <li><a href="">News</a></li>
                     <li><a href="">Rencontres</a></li>
                     <li><a href="">Croquettes</a></li>
