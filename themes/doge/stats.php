0a64e7d272e6cafeced715edb21dacb75440c252
Djex <djex@mail.com>
2014-11-16 21:24:54 +0100

    lol je commité

--- themes/doge/stats.php
+++ themes/doge/stats.php
@@ -4,21 +4,24 @@ Template Name: Statistiques
 */
 ?>
 <?php get_header(); ?>
-<div class="main page">
-  <?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-      <div class="post">
-        <h1 class="post-title"><?php the_title(); ?></h1>
-        <div class="post-content">
-		  <?php the_content(); ?>
-          <p>Nombre de Posts : <strong><?php echo wp_count_posts()->publish; ?></strong></p>
-          <p>Nombre de Pages : <strong><?php echo wp_count_posts('page')->publish; ?></strong></p>
-          <p>Nombre de commentaires publiés : <strong><?php echo wp_count_comments()->approved; ?></strong></p>
+<div id="stats_page">
+    <div class="main single clearfix">
+        <div class="col70">
+            <?php if (have_posts()) : ?>
+                <?php while (have_posts()) : the_post(); ?>
+                    <div class="post">
+                        <h1 class="post-title"><?php the_title(); ?></h1>
+                        <div class="post-content">
+                            <?php the_content(); ?>
+                            <p>Nombre de Posts : <strong><?php echo wp_count_posts()->publish; ?></strong></p>
+                            <p>Nombre de Pages : <strong><?php echo wp_count_posts('page')->publish; ?></strong></p>
+                            <p>Nombre de commentaires publiés : <strong><?php echo wp_count_comments()->approved; ?></strong></p>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
         </div>
-      </div>
-    <?php endwhile; ?>
-  <?php endif; ?>
+        <div class="col30 sidebar"><?php get_sidebar(); ?></div>
+    </div>
 </div>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
-
--- themes/doge/template-a.php
+++ themes/doge/template-a.php
@@ -3,22 +3,24 @@
   Template Name: Template A
   Description: Une page classique contenu gauche widget à droite
 */
-
-get_header(); ?>
-
-<div class="main page">
-  <?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-      <div class="post">dsds
-        <h1 class="post-title"><?php the_title(); ?></h1>
-        <div class="post-content">
-		  <?php the_content(); ?>
+ get_header(); ?>
+<div id="single_event_page" class="clearfix">
+    <div class="main single clearfix">
+        <div class="col70">
+            <?php if (have_posts()) : ?>
+                <?php while (have_posts()) : the_post(); ?>
+                    <div class="post">
+                    <h1 class="post-title"><?php the_title(); ?></h1>
+                    <div class="post-content">
+                        <?php the_content(); ?>
+                    </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
         </div>
-      </div>
-    <?php endwhile; ?>
-  <?php endif; ?>
+        <div class="col30 sidebar"><?php get_sidebar(); ?></div>
+    </div>
 </div>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
 
 
